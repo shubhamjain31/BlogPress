12a17a7cb6962c1e80f42a36f0c552348f017cdc
shubhamjain31 <sj27754@gmail.com>
2021-07-23 22:19:45 +0530

    Added Blog App functionality

--- App/models.py
+++ App/models.py
@@ -1,3 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
+from froala_editor.fields import FroalaField
+
+from .helpers import *
 
 # Create your models here.
+
+class Profile(models.Model):
+    user 			= models.ForeignKey(User, on_delete=models.CASCADE)
+    is_verified 	= models.BooleanField(default=False)
+    token 			= models.CharField(max_length=100)
+    mobile 			= models.CharField(max_length=15)
+
+    def __str__(self):
+        return str(self.user)
+    
+
+class BlogModel(models.Model):
+	user 			= models.ForeignKey(User, blank=True , null=True , on_delete=models.CASCADE)
+	title 			= models.CharField(max_length=1000)
+	content 		= FroalaField()
+	slug 			= models.SlugField(max_length=1000 , null=True , blank=True)
+	image 			= models.ImageField(upload_to='blog')
+	created_at 		= models.DateTimeField(auto_now_add=True)
+	upload_to 		= models.DateTimeField(auto_now=True)
+	browser 		= models.CharField(max_length=200)
+	ip_address		= models.CharField(max_length=100)
+
+	def __str__(self):
+	    return self.title
+
+	def save(self , *args, **kwargs): 
+	    self.slug = generate_slug(self.title)
+	    super(BlogModel, self).save(*args, **kwargs)
+    
+    
+    
+    
+    
+
+
--- BlogPress/settings.py
+++ BlogPress/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-n-8e*x7e54-t@dasn$ksb4@c9$0)==n(wv=ap5ir3%ywx$y+b6'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -37,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'froala_editor',
+    'rest_framework',
+    'App'
 ]
 
 MIDDLEWARE = [
@@ -118,8 +123,25 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
+
+STATICFILES_DIR = {
+    os.path.join(BASE_DIR , "App/static")
+}
+
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media') 
+MEDIA_URL = '/media/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = config('EMAIL_PORT')
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
